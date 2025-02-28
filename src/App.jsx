d31f4d5abf462155661c0453af8f79a7ae5bc17d
soumyayalahankaajjegowdagit config --global user.name soumyayalahankaajjegowda <soumya.ajjegowda@gmail.com>
2025-02-28 14:36:02 -0700

    uplaodign code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-
 import './App.css'
+import Home from "./pages/Home"
 
 function App() {
- 
+
 
   return (
     <div>
-
-
+     <Home />
+     
     </div>
    
   )
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,23 @@
-
 function MovieCard({ movie }) {
+
+
+    function onFavoriteClick() {
+      alert("clicked")
+    }
   return<div className="movie-card">
     <div classNamer="movie-poster">
       <image src={movie.url} alt={movie.title}/>
       <div className="movie-overlay">
-        <button className="favrite-btn" onClick={}></button>
+        <button className="favrite-btn" onClick={}>
+           ♥
+        </button>
+         
       </div>
+    </div>
+    <div className="movie-info">
+      <h3>{movie.title}</h3>
+      <p>{movie.release_date}</p>
     </div>
   </div>
 }
+export default MovieCard;
